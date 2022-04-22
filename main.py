bf7985cebcef907b97f6c93e87dfd548d77191f4
Israel Zexer <israzex777@gmail.com>
2022-04-22 20:55:09 +0300

    expert, zig-zag, break bots were implemented

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from flight_recording import record_flight_for_seconds
 from bots import DroneBot
 
 if __name__ == "__main__":
-    # record_flight_for_seconds(20, f"new_manual_controller_{datetime.datetime.now().strftime('%Y:%m:%d_%H:%M:%S')}")
+    record_flight_for_seconds(40, f"manual_{datetime.datetime.now().strftime('%Y:%m:%d_%H:%M:%S')}")
