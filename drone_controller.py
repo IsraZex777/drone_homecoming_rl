e39b3e3035d73fc7376198e4f8642ae86884127e
Israel Zexer <israzex777@gmail.com>
2022-05-01 01:05:09 +0300

    feat: Actor drone controller with basic functionality was implemented

--- drone_controller.py
+++ drone_controller.py
@@ -87,3 +87,23 @@ class DroneController:
 
             self.desired_velocity = action_to_velocity[action]
             self.move(self.desired_velocity, action_to_yaw_rate[action])
+
+
+class AgentDroneController:
+    def __init__(self, drone_name: str = ""):
+        self._controller = DroneController(drone_name=drone_name)
+
+    def apply_action_for_seconds(self, action: DroneActions, duration: float = 1):
+        curr_timestamp = time.time()
+        while time.time() - curr_timestamp < duration:
+            self._controller.handle_action(action)
+            time.sleep(.2)
+
+    def handle_action(self, action: DroneActions, duration: float = 1, stop_duration: float = 5.5):
+        stop_actions = [DroneActions.UP, DroneActions.DOWN, DroneActions.FORWARD, DroneActions.BACKWARD]
+        self.apply_action_for_seconds(action, duration)
+
+        # Waits till drone completely stops
+        if action in stop_actions:
+            time.sleep(stop_duration)
+
