ec26f47d0f4e15ec9dc83c6aebbee863c67f335f
Israel Zexer <israzex777@gmail.com>
2022-05-28 13:46:04 +0300

    fix: Distance sensor tuning, reward and other fixes

--- actor_model.py
+++ actor_model.py
@@ -74,8 +74,7 @@ def make_actor_action(actor_model: tf.keras.Model,
 
     noise = noise_object()
     # Adding noise to action
-    sampled_actions = abs(action_duration.numpy()[0] * 10 + noise)
-    # print(sampled_actions)
-    # We make sure action is within bounds
-
+    sampled_actions = abs(action_duration.numpy()[0] * action_duration_upper_limit + noise)
+    sampled_actions = max(sampled_actions, action_duration_lower_limit)
+    
     return action_type_vector, sampled_actions
